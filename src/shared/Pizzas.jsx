a83fc91dc96cee148199cc8a72ffd5a953f236b8
isabela00123 <isabelasdiasmendes34@gmail.com>
2024-08-08 21:07:15 -0300

    tudo acabará em pizza.

---
+++ src/shared/Pizzas.jsx
@@ -0,0 +1,40 @@
+import margharita from "../assets/images/margharita.png"
+import peperoni from "../assets/images/peperoni.png"
+import queijo from "../assets/images/queijo.png"
+import frango from "../assets/images/frango.png"
+import palmito from "../assets/images/palmito.png"
+
+const Pizzas = [
+    {
+        "id": 1,
+        "nome": "Margherita",
+        "descrição": "Molho de tomate, mussarela, manjericão fresco e azeite de oliva.",
+        "imagem": margharita
+      },
+      {
+        "id": 2,
+        "nome": "Pepperoni",
+        "descrição": "Molho de tomate, mussarela e fatias de pepperoni.",
+        "imagem": peperoni
+      },
+      {
+        "id": 3,
+        "nome": "Queijo",
+        "descrição": "Molho de tomate, mussarela, parmesão, gorgonzola e provolone.",
+        "imagem": queijo
+      },
+      {
+        "id": 4,
+        "nome": "Frango com Catupiry",
+        "descrição": "Molho de tomate, frango desfiado, catupiry e cebola.",
+        "imagem": frango
+      },
+      {
+        "id": 5,
+        "nome": "Palmito",
+        "descrição": "Molho de tomate, mussarela, calabresa fatiada e cebola.",
+        "imagem": palmito
+      },
+]
+
+export default Pizzas;
